3a0a5690f9428d57864873720ba58710d7ef95a4
Kyle Youn <113752652+Kyle-Youn@users.noreply.github.com>
2024-12-08 02:17:10 +0900

    Update and rename redirection.py to redirection.sh

---
+++ redirection.sh
@@ -0,0 +1,14 @@
+# 명령어의 표준출력을 파일로 저장하기(덮어쓰기)
+command > output.txt
+
+# 명령어의 표준출력을 파일에 저장하기(추가하기)
+command >> output.txt
+
+# 표준입력을 파일에서 가져오기
+command < input.txt
+
+# 명령어의 표준에러를 파일로 저장하기(덮어쓰기)
+command 2> output.txt
+
+# 명령어의 표준에러를 파일에 저장하기(추가하기)
+command 2>> output.txt
