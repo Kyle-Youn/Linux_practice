367ceb64d28d4769473c185bc17f63c9ac0f6d55
Kyle Youn <113752652+Kyle-Youn@users.noreply.github.com>
2025-06-13 23:58:48 +0900

    Create stat.sh

---
+++ stat.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+stat example.txt
+
+#   File: example.txt
+#   Size: 0               Blocks: 0          IO Block: 4096   regular empty file
+# Device: 179,2   Inode: 128000      Links: 1
+# Access: (0664/-rw-rw-r--)  Uid: ( 1000/    june)   Gid: ( 1003/    june)
+# Access: 2025-06-13 23:56:57.670393164 +0900
+# Modify: 2025-06-13 23:56:57.670393164 +0900
+# Change: 2025-06-13 23:56:57.670393164 +0900
+#  Birth: 2025-06-13 23:56:57.670393164 +0900
